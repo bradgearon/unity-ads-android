c26c055acdef8fbefa570712a5aca536ae301e8d
Brad Gearon <bradgearon@gmail.com>
2016-12-07 04:37:10 -0600

    updated version

--- copy-build.cmd
+++ copy-build.cmd
@@ -1,1 +1,2 @@
-copy /y lib\build\outputs\aar\UnityAds-release.aar ..\pfudor\Assets\Plugins\Android\UnityAds.aar
+copy /y lib\build\outputs\aar\UnityAds-release.aar ..\pfudor\Assets\Plugins\Android\UnityAds.aar  
+copy /y lib\build\outputs\aar\UnityAds-release.aar ..\divisors\Assets\Plugins\Android\UnityAds.aar  
